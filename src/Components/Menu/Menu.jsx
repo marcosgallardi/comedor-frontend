ecb87867a2dd622af8af18427cda251dd1b7055d
marcosgallardi <100347619+marcosgallardi@users.noreply.github.com>
2023-06-29 12:51:20 -0300

    Update Menu.jsx

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -12,7 +12,7 @@ export const Menu = ({ diaComida, nombre }) => {
 
           </div>
         </div>
-        <div className="col-6 col-sm-10 text-start fs-4">
+        <div className="col-6 col-sm-10 text-start fs-4 mt-4">
           {nombre}: <span className="fs-5 text-primary mt-4">{diaComida}</span>
         </div>
       </div>
